57b1dd9ba00683c89b3b6db7160e18e9073ee992
thanha-2610 <duongthanhnha2610@gmail.com>
2026-01-18 21:40:23 +0700

    init shadcn

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fcf40abc8c2f7640"; }
+/******/ 		__webpack_require__.h = function() { return "60362d493a481b36"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
